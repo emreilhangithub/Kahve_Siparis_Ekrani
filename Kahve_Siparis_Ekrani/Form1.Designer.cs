588c91734de41281852676c70db462da651752ad
Mustafa Emre Ilhan <mustafaemreilhan1903@gmail.com>
2021-05-31 00:06:43 +0300

    Login paneli giriş işlemi başarılı bir şekilde tamamlandı sıra form 2de

--- Kahve_Siparis_Ekrani/Form1.Designer.cs
+++ Kahve_Siparis_Ekrani/Form1.Designer.cs
@@ -40,66 +40,81 @@ namespace Kahve_Siparis_Ekrani
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(179, 103);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(238, 149);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(93, 20);
+            this.label1.Size = new System.Drawing.Size(135, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "Kullanıcı Adı";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(179, 161);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(238, 233);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(42, 20);
+            this.label2.Size = new System.Drawing.Size(57, 29);
             this.label2.TabIndex = 1;
             this.label2.Text = "Şifre";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(183, 230);
+            this.button1.Location = new System.Drawing.Point(244, 334);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(129, 55);
+            this.button1.Size = new System.Drawing.Size(172, 79);
             this.button1.TabIndex = 2;
             this.button1.Text = "Giriş Yap";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(395, 230);
+            this.button2.Location = new System.Drawing.Point(526, 334);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(158, 55);
+            this.button2.Size = new System.Drawing.Size(210, 79);
             this.button2.TabIndex = 3;
             this.button2.Text = "Çıkış Yap";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(362, 103);
+            this.textBox1.Location = new System.Drawing.Point(483, 149);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(191, 26);
+            this.textBox1.Size = new System.Drawing.Size(253, 37);
             this.textBox1.TabIndex = 4;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(362, 155);
+            this.textBox2.Location = new System.Drawing.Point(483, 224);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(191, 26);
+            this.textBox2.Size = new System.Drawing.Size(253, 37);
             this.textBox2.TabIndex = 5;
+            this.textBox2.UseSystemPasswordChar = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Brown;
+            this.ClientSize = new System.Drawing.Size(1136, 624);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Giriş Ekranı";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Kahve_Siparis_Ekrani/Form1.cs
+++ Kahve_Siparis_Ekrani/Form1.cs
@@ -7,14 +7,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Sql; 
+using System.Data.OleDb; 
+using System.Data.SqlClient;
 
 namespace Kahve_Siparis_Ekrani
 {
     public partial class Form1 : Form
     {
+
+        SqlConnection con = new SqlConnection();
+
         public Form1()
         {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "Data Source=DESKTOP-E9UTSVL;Initial Catalog=KahveSiparis;Integrated Security=True";
+
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-E9UTSVL;Initial Catalog=KahveSiparis;Integrated Security=True");
+            con.Open();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "Data Source=DESKTOP-E9UTSVL;Initial Catalog=KahveSiparis;Integrated Security=True";
+            con.Open();
+            string userid = textBox1.Text;
+            string password = textBox2.Text;
+            SqlCommand cmd = new SqlCommand("select UserName,password from login where UserName='" + textBox1.Text + "'and password='" + textBox2.Text + "'", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                MessageBox.Show("Giriş başarılı Ana Sayfaya Hoş Geldiniz");
+            
+            }
+            else
+            {
+                MessageBox.Show("Geçersiz Giriş, lütfen kullanıcı adı ve şifreyi kontrol edin");
+            }
+            con.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        
     }
 }
